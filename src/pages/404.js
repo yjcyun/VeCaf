0f2774d659f22f75d69c36c4fcb004fec65f3297
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-09-29 09:38:45 -0400

    Add an error page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100vh'}}>
+      <h1>Sorry! This page doesn't exist</h1>
+    </div>
+  )
+}
+
+export default Error
